16646ca73045341b21d51f41164ba2f901ca1355
Christopher G <zeus1400@googlemail.com>
2018-05-24 20:09:35 +0200

    edit structure

---
+++ php/getKeywords.php
@@ -0,0 +1,57 @@
+<?php
+
+//Verbindung aufbauen
+include('conn.php');
+
+function getMain($connection)
+{
+    getKeywordsCBR($connection);
+}
+
+
+function getKeywordsCBR($connection)
+{
+    $sqlStmt = "SELECT CBR_Keyword_DE.id, CBR_Keyword_DE.name, CBR_ICF_Kategorie.id AS katid ,CBR_ICF_Kategorie.DE FROM CBR_Keyword_DE, CBR_ICF_Kategorie WHERE CBR_Keyword_DE.ICFkat = CBR_ICF_Kategorie.id ORDER BY CBR_ICF_Kategorie.id;";
+        
+        $result =  mysqli_query($connection,$sqlStmt);
+        $data = array();
+        if ($result = $connection->query($sqlStmt)) 
+        {
+            while ($row = $result->fetch_assoc()) 
+            {
+                $id = $row["id"];
+                $name = $row["name"];	
+				$katid = $row["katid"];
+				$DE = $row["DE"];
+				
+				
+                array_push($data,array("ID"=> $id,"name"=>$name,"katid"=>$katid,"DE"=>$DE));  
+            }
+            
+            // Objekt freigeben
+            $result->free();
+        }
+  
+  
+        // Ausgabe
+        foreach ($data as $d)
+        {
+            $id = $d["ID"];
+            $name = $d["name"];
+			$katid = $d["katid"];
+			$DE = $d["DE"];
+          
+            echo utf8_encode("$id(//separatormid//)$name(//separatormid//)$katid(//separatormid//)$DE");
+            echo "[||separatorend||]";
+        }
+		
+		closeConnection($connection);
+  
+  
+}
+ 
+//Verbindung schlieÃŸen.
+function closeConnection($connection){
+  mysqli_close($connection);
+}
+?>
