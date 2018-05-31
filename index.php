ff4b6835b07b7d3ae0d4408d97f6105130cea0e5
Christopher G <zeus1400@googlemail.com>
2018-05-31 11:40:51 +0200

    excluded the Navbar and made diagnose a php file

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 include_once 'php/include/conn.php';
 include_once 'php/include/functions_login.php';
+
  
 sec_session_start();
  
@@ -31,51 +32,17 @@ if (login_check($mysqli) == true) {
 
         <link href="css/style.css" rel="stylesheet">
 
+
     </head>
 
     <body id="home">
-
-        <!-- _______________________________________NavBar_____________________________________________________-->
-
-        <nav class="navbar-default navbar-fixed-top" role="navigation">
-            <!-- role für browser zur                                                                            erkennung-->
-            <div class="container">
-                <div class="navbar-header ">
-                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
-                         <span class="sr-only">Toggle navigation</span>
-                         <span class="icon-bar"></span>
-                         <span class="icon-bar"></span>
-                         <span class="icon-bar"></span>
-                        <!-- erzeugen die 3 striche-->
-                    </button>
-                     <a class="navbar-brand" href="#home">MedAusbild</a> 
-                    <!-- Titel der Navbar-->
-                </div>
-
-                <div class="collapse navbar-collapse navbar-right navbar-ex1-collapse">
-                    <ul class="nav navbar-nav">
-                        <li class="menuItem"><a href="#...">Diagnose</a>
-                        </li>
-                        <li class="menuItem"><a href="#...">Quiz</a>
-                        </li>
-                        <li class="menuItem"><a href="#...">Checker</a>
-                        </li>
-
-                        <li class="menuItem"><a href="#...">Classroom</a>
-                        </li>
-                        <li class="menuItem"><a href="#...">Forum</a>
-                        </li>
-                        <li class="menuItem"><a href="#contact">Kontakt</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-            <!--container-->
-        </nav>
-        
 		
+		<!-- include Navbar -->
+		<?php
+			include ("php/include/navbar.php");
+		?>
 
-		<!-- ________________________________________________________________________________________________________ -->
+		
 
 
         <!-- _________________________Content________________________________-->
---
+++ php/include/navbar.php
@@ -0,0 +1,43 @@
+<nav class="navbar-default navbar-fixed-top" role="navigation">
+	<!-- role für browser zur                                                                            erkennung-->
+	<div class="container">
+		<div class="navbar-header ">
+			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<!-- erzeugen die 3 striche-->
+			</button>
+			<a class="navbar-brand" href="index.php">MedAusbild</a>
+			<!-- Titel der Navbar-->
+		</div>
+
+		<div class="collapse navbar-collapse navbar-right navbar-ex1-collapse">
+			<ul class="nav navbar-nav">
+				<li class="menuItem">
+					<a href="diagnose_seite.php">Diagnose</a>
+				</li>
+				<li class="menuItem">
+					<a href="#...">Quiz</a>
+				</li>
+				<li class="menuItem">
+					<a href="#...">Checker</a>
+				</li>
+
+				<li class="menuItem">
+					<a href="#...">Classroom</a>
+				</li>
+				<li class="menuItem">
+					<a href="#...">Forum</a>
+				</li>
+				<li class="menuItem">
+					<a href="#contact">Kontakt</a>
+				</li>
+			</ul>
+		</div>
+	</div>
+	<!--container-->
+</nav>
+
+
