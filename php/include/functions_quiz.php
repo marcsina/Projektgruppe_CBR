2d882ff0d76e198454c05e72482c447e94d43256
Christopher G <zeus1400@googlemail.com>
2018-08-02 15:28:11 +0200

    fixed generateFourQuestions

--- php/include/functions_quiz.php
+++ php/include/functions_quiz.php
@@ -253,9 +253,9 @@ function checkCorrectAnswer($givenAnswer, $correctAnswer)
 
 function genereateFourQuestionsMultiplayer($mysqli)
 {
-	for($questionCounter = 0; $questionCounter < 4; $questionCounter++)
+	for($questionCounter = 0; $questionCounter < 5; $questionCounter++)
 	{
-		//Load random Question Type High with 50% probability and Low with 50% probability
+        //Load random Question Type High with 50% probability and Low with 50% probability
 		if( rand(0,1) == 0)
 		{
 			$question = loadRandomQuestionHigh($mysqli);
@@ -266,16 +266,14 @@ function genereateFourQuestionsMultiplayer($mysqli)
 			$type = 1;
 		}
 
-		if ($insert_stmt = $mysqli->prepare("INSERT INTO `MP_FRAGE`(`Type`, `Casename`, `Correct_A1`, `A2`, `A3`, `A4`) VALUES (?,?,?,?,?,?)")) {
+		if ($insert_stmt = $mysqli->prepare("INSERT INTO `MP_FRAGE`(`Type`, `Casename`, `Correct_A1`, `A2`, `A3`, `A4`) VALUES (?,?,?,?,?,?)"))
+        {
+            echo "3";
             $insert_stmt->bind_param('isssss', $type, $question['casename'], $question['antwort1'], $question['antwort2'], $question['antwort3'], $question['antwort4'] );
             // Führe die vorbereitete Anfrage aus.
-            if (! $insert_stmt->execute()) {
-			//Fehler beim EIntrag in DB
-                return false;
-            }
+            $insert_stmt->execute();
         }
-
-	}
+    }
 }
 
 if(isset($_POST['antwort1_Button'], $_POST['correctanswer']))
