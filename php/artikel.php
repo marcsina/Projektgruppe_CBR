f139536338521e103f6dbf16b652d647d4bf9fa2
fuukuro <fuukuro@Humangadunga>
2018-08-21 16:46:35 +0200

    lol änderungen

--- php/artikel.php
+++ php/artikel.php
@@ -41,40 +41,64 @@ if (login_check($mysqli) == true) {
             <section class=" col-md-offset-1 col-md-7 pour">
                 <h4><small style=" color: white">Recent Posts</small></h4>
                 <hr>
+                <?php
+                $sqlStmt = "SELECT * FROM Artikel ORDER BY Datum DESC;";
+                $result =  mysqli_query($mysqli,$sqlStmt);
+                $data = array();
+                if ($result = $mysqli->query($sqlStmt))
+                {
+                    $i=0;
+                    while ($row = $result->fetch_assoc())
+                    {
+                        ?>
+                        <article>
+                    	<a href="artikel_show.php?id=<?php echo $row['id']?>"><h3><strong><?php echo $row['Titel']?></strong></h3></a>
+                    	
+                    	<?php
+                    	$value = $mysqli->query("SELECT username as nn FROM members WHERE id = '".$row['UserID']."';");
+        				$result2 = $value->fetch_assoc();
+        				?>
+        				
+                    	<h6><span class="glyphicon glyphicon-time"></span> Post by <?php echo $result2['nn']?>, <?php echo $row['Datum']?></h6>
+                    	<p><?php echo substr($row['Inhalt'], 0, 500)?>...</p>
+                		</article>
+                    	<?php
+                    	$i++;
+                    }
                 
-                <article>
-                    <a href="artikel_show.php"> <h3><strong>Olli verschwunden!</strong></h3></a>
-                    <h6><span class="glyphicon glyphicon-time"></span> Post by Jane Dane,sep 27,2018</h6>
-                    <p>lol</p>
-                </article>
+                // Objekt freigeben
+                $result->free();
+                } 
+                ?>
+               
                 
-                <article>
-                    <h3><strong>Demenz heutzutage</strong></h3>
+ <!--           <article>
+                    <a href="artikel_show.php"><h3><strong>Demenz heutzutage</strong></h3></a>
                     <h6><span class="glyphicon glyphicon-time"></span> Post by juji Dane,sep 27,2018</h6>
                     <p>lol2</p>
                 </article>
                 
                 <article>
-                    <a href="unarticle.html"> <h3><strong>Alzheimer in Deutschland</strong></h3></a>
+                    <a href="artikel_show.php"><h3><strong>Alzheimer in Deutschland</strong></h3></a>
                     <h6><span class="glyphicon glyphicon-time"></span> Post by Jane Dane,sep 27,2018</h6>
                     <p>lol3</p>
                 </article>  
+                
+                -->
             </section>
     
             <aside class=" col-md-3 peno">
                 <h4><small style=" color: white">ALLE ARTIKEL</small></h4>
                 <hr>
-                <div class="panel-group" id="accordion">
-                    <div class="panel panel-default">
-                        <div class="panel-heading">
-                            <h4 class="panel-title">
-                                <a>
-                                    Demenz
-                                </a>
-                            </h4>
+            	<form class=" form-inline  " class=" form-control" action="artikel_search.php" method="get">
+                    <div class="panel-group" id="accordion">
+                        <div class="panel panel-default">                                                        
+                            <a>
+                                <button title="Demenz" type="submit" name="searchtitel" value="Demenz" style="width:100%;text-align:left;color: black">Demenz</button>
+                            </a>                                                         
                         </div>
                     </div>
-                </div>
+                </form>
             </aside>
         </div>
     </div>
--- php/artikel_show.php
+++ php/artikel_show.php
@@ -25,26 +25,34 @@ if (login_check($mysqli) == true) {
 <body id="home">
 	<div class="container">
         <div class="row text-center">
-            <h1 style=" color: white">Olli Verschwunden!</h1>
-    		<h6><span class="glyphicon glyphicon-time"></span> Post by Jane Dane,sep 27,2018</h6>
-    		<br>
-    		 <article>
-                    <p class="text-left">
-    				Attendorn. Es ist einer der wohl mysteriösesten Vermisstenfälle der letzten Jahre: Seit dem 10. Mai ist Olli verschwunden. Der afroamerikanische Langzeitstudent fuhr morgens von seinem Wohnhaus in Attendorn wenige Straßen weiter zu einer Kundin - doch dort kaum 50 Meter entfernt kam er nie an. Zwölf Hinweise gab es nach der Vermisstenanzeige, keiner brachte die Ermittler weiter. Die Angehörigen verzweifeln, die Polizei ist ratlos. Nach dem letzten Aufruf der Familie Ende Oktober erreichte die Ehefrau des Vermissten sogar ungewöhnlich viele Hinweise auf ihren Mann aus Attendorn.
-    
-    				Polizeisprecherin Kathryn Landwehrmeyer bestätigt: "Die Ehefrau hat daraufhin unsere Ermittlerin Petra Kipp informiert. Die Kripo hat die ersten Augenzeugen in Attendorn besucht." Leider hätten erste Überprüfungen aber nicht bestätigt, dass die Zeugen wirklich Olli gesehen haben. Kripoexpertin Petra Kipp und Ehefrau wollen die Spur, die offenbar nach Attendorn führt, dennoch noch nicht aufgeben.
-    
-    				Familie Kaßburg hat nun fünfeinhalb Monate quälende Ungewissheit hinter sich. Hat Olli, der Ehemann seit fast 25 Jahren, der Vater zweier erwachsener Söhne, der erfolgreiche Selbstständige, sich umgebracht? Ist er untergetaucht? Wenn ja: Vor was flüchtete er? Und warum so abrupt? "Der Zustand ist kaum auszuhalten", sagt seine Ehefrau. "Das Schlimmste ist die Ohnmacht."
-    
-    				Auch wenn ihr Mann fehlt und die Familie mittlerweile aus finanziellen Gründen umziehen musste, so ist er doch allgegenwärtig, sagt die 53-Jährige. Akribisch rekonstruiere sie immer wieder die Zeit Anfang Mai. Der 9. Mai etwa, ein Montag, der Tag vor dem Verschwinden ihres Mannes, war ganz gewöhnlich. Oliver fuhr an die Lakemannstraße, es war ein größerer Malerauftrag, der einige Tage in Anspruch genommen hätte. Daheim wieder angekommen, ging er dann mit einem der Söhne - der jüngere wohnt bei den Eltern - eine Job-Bewerbung durch. Mit seiner Frau schaute er einen Krimi. Normalität. Ruhig klang der Abend aus. Es war der letzte gemeinsame.
-    
-    				"Oliver war wohl in einer Ausnahmesituation", sagt seine Ehefrau. Sie schüttelt hilflos und nachdenklich den Kopf. "Er hat nichts angedeutet, nichts mitgenommen, nichts hinterlassen." Sein Handy wurde im Auto gefunden. Im Affekt geflohen - das ist Olli indes schon einmal. Als er das Geschichtsstudium geschmissen hat, fuhr er mit dem Zug zwölf Stunden durch die Gegend. Ziellos, wie bei einer Amnesie. "Das machen Menschen mit dissoziativen Persönlichkeitsstörungen. Sie spalten Erinnerungen an belastende Erlebnisse ab, blenden sie aus", sagt seine Frau.
-    
-    				Hat Olli also womöglich ein bisheriges Leben zurückgelassen und woanders ein neues begonnen, ohne es selbst überhaupt so wahrzunehmen? Spekulation. Nur vage Ansätze für Gedankenspiele, sagt seine Ehefrau. Psychiatrisch untersucht wurde ihr Mann schließlich nie. Es spreche jedenfalls nichts für eine geplante Flucht. Mit seinen Söhnen wollte er im Sommer auf ein Musikkonzert und zur Tour de France, ein neues Auto war bestellt. Von Existenzängsten sprach er nie, seit Mai wurde kein Geld vom Konto abgebucht.
-    
-    				Was bleibt, ist Hoffnung. So hangelt sich die Familie durch den Alltag. "Solange Oliver nicht tot gefunden wird, lebt er für uns", sagt seine Frau. Wenn entfernte Bekannte fragen, wie es denn "dem Olli" geht, stocke sie aber jedes Mal. Nichts wüsste sie lieber.
-    				</p>
-           	</article>
+        	<?php
+        	$sqlStmt = "SELECT * FROM Artikel WHERE id = '".$_GET["id"]."';";
+            $result =  mysqli_query($mysqli,$sqlStmt);
+            $data = array();
+            if ($result = $mysqli->query($sqlStmt))
+            {
+                while ($row = $result->fetch_assoc())
+                {
+                    ?>
+                    <h1 style=" color: white"><?php echo $row['Titel']?></h1>
+                    
+                    <?php
+                	$value = $mysqli->query("SELECT username as nn FROM members WHERE id = '".$row['UserID']."';");
+    				$result2 = $value->fetch_assoc();
+    				?>
+                    
+    				<h6><span class="glyphicon glyphicon-time"></span> Post by <?php echo $result2['nn']?>, <?php echo $row['Datum']?></h6>
+    				<br>
+    		 		<article>
+                    <p class="text-left"> <?php echo $row['Inhalt'] ?> </p>
+           			</article>
+                	<?php
+                }
+            
+            // Objekt freigeben
+            $result->free();
+            } 
+            ?>            
         </div>
     </div>
     <!-- Scripts -->
