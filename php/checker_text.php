f5d4d676a7419fb904201af47332e7da37c79d12
fuukuro <fuukuro@Humangadunga>
2018-08-23 18:32:12 +0200

    Charts angepasst

--- php/checker_text.php
+++ php/checker_text.php
@@ -196,13 +196,16 @@ if (login_check($mysqli) == true) {
                         label: 'Results in %',
                         data: [p1,p2,p3,p4],
                         backgroundColor: [
-                            'rgba(255, 99, 132, 0.2)',
-                            'rgba(54, 162, 235, 0.2)'
+                        	'rgba(255, 99, 132, 0.6)',
+                            'rgba(54, 162, 235, 0.6)',
+                            'rgba(255, 206, 86, 0.6)',
+                            'rgba(75, 192, 192, 0.6)'
                         ],
                         borderColor: [
-                            'rgba(255,99,132,1)',
+                        	'rgba(255,99,132,1)',
                             'rgba(54, 162, 235, 1)',
-                            'rgba(255, 206, 86, 1)'
+                            'rgba(255, 206, 86, 1)',
+                            'rgba(75, 192, 192, 1)'
                         ],
                         borderWidth: 1
                     }]
