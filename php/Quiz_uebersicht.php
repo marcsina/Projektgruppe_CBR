21874b09cccc30972af99540ea391372d142d8b9
Oliver Koch <rofldofl2@googlemail.com>
2018-08-24 17:14:18 +0200

    Implemented autocomplete for Quiz_uebersicht

--- php/Quiz_uebersicht.php
+++ php/Quiz_uebersicht.php
@@ -24,7 +24,7 @@ if (login_check($mysqli) == true) {
 
     <!-- Bootstrap core CSS -->
     <link href="../css/bootstrap.min.css" rel="stylesheet" />
-
+	<link href="../css/style_autocomplete.css" rel="stylesheet">
     <link href="../css/style3.css" rel="stylesheet" />
     <link href="../css/Quiz_uebersicht.css" rel="stylesheet" />
 
@@ -232,9 +232,9 @@ include ("include/navbar.php");
 
 
 
-                <div class=" col-lg-12 col-md-12 col-sm-12">
+                <div class=" col-lg-12 col-md-12 col-sm-12" >
                     
-                    <?php
+                    <?php					
                         $array = getAllMembers($mysqli);
                         $array2 = getAlreadyChallengedUsers($mysqli, $_SESSION['user_id']);
                         $array3 = getPendingChallengesUsers($mysqli, $_SESSION['user_id']);
@@ -243,38 +243,64 @@ include ("include/navbar.php");
                         $check = 0;
 
 
-                        echo "<h3 class='Multip' style='text-align: center;'>
+                        echo "	<h3 class='Multip' style='text-align: center;'>
                                         <b>
                                             <u>Leute die du herausfordern kannst</u>
                                         </b>
-                              </h3>
-                                    <table class='table table-striped'>
-                                        <thead>
-                                            <tr>
-                                                <th scope='col'>#</th>
-                                                <th scope='col'>Benutzername</th>
-                                                <th scope='col' style='text-align:center;'>Status</th>
-                                            </tr>
-                                        </thead>
-                                    <tbody>";
+								</h3>
 
+								<form autocomplete='off'>
+        							<input type='text' name='suche' id='input_category' placeholder='Suchen' class='form-control ' value=''>
+        						</form>
+
+                                <table class='table table-striped' id = 'table_user'>
+                                    <thead>
+                                        <tr>
+                                            <th scope='col'>#</th>
+                                            <th scope='col'>Benutzername</th>
+                                            <th scope='col' style='text-align:center;'>Status</th>
+                                        </tr>
+                                    </thead>
+                                <tbody>";
+
+						//Convert data for autocomplete to javascript
+						
+						$js_array = json_encode($array);
+						$js_getAlreadyChallengedUsers = json_encode($array2);
+						$js_getPendingChallengesUsers = json_encode($array3);
+						$js_checkCurrentMPGamesForChallenges = json_encode($array4);
+						$js_sessionID = json_encode($_SESSION['user_id']);			
+
+						echo "
+						<script type='text/javascript'>
+
+						var javascript_array = ".$js_array.";
+						var alreadyChallengedUsers_array = ".$js_getAlreadyChallengedUsers.";
+						var pendingChallengesUsers_array = ".$js_getPendingChallengesUsers.";
+						var currentMPGamesForChallenges_array = ".$js_checkCurrentMPGamesForChallenges.";
+						var sessionID = ".$js_sessionID.";
+						
+						</script>
+						";
+
+						// Convert end
 
                         //alle nutzer
 						$count = 0;
-                        foreach($array as &$user )
-                        {
+						foreach($array as &$user )
+						{
 							$message="";
-                            //sind wirs selber?
-                            if($_SESSION['user_id'] != $user['id'])
-                            {
-                                //alle von uns herausgeforderten
-                                foreach($array2 as &$alreadyChallengedUsers)
-                                {
-                                    if($user['id'] == $alreadyChallengedUsers['userID2'])
-                                    {
-                                        //User schon herausgefordert
-                                        if($check == 0)
-                                        {
+							//sind wirs selber?
+							if($_SESSION['user_id'] != $user['id'])
+							{
+								//alle von uns herausgeforderten
+								foreach($array2 as &$alreadyChallengedUsers)
+								{
+									if($user['id'] == $alreadyChallengedUsers['userID2'])
+									{
+										//User schon herausgefordert
+										if($check == 0)
+										{
 											echo "
 												<tr>
 
@@ -290,47 +316,47 @@ include ("include/navbar.php");
 												</tr>
 											";
 											$check = 1;
-                                        }
-                                    }
-                                    else
-                                    {
-                                        //Dododo
-                                    }
-                                }
+										}
+									}
+									else
+									{
+										//Dododo
+									}
+								}
 
-                                foreach($array3 as &$userChallengesUs)
-                                {
-                                    if($user['id'] == $userChallengesUs['userID1'])
-                                    {
-                                        //User schon herausgefordert
-                                        if($check == 0)
-                                        {
-                                            //echo "<li>".$user['username']." hat dich bereits aufgefordert. Nimm an</li>";
-                                            $check = 1;
-                                        }
-                                    }
-                                    else
-                                    {
-                                        //Dododo
-                                    }
-                                }
+								foreach($array3 as &$userChallengesUs)
+								{
+									if($user['id'] == $userChallengesUs['userID1'])
+									{
+										//User schon herausgefordert
+										if($check == 0)
+										{
+											//echo "<li>".$user['username']." hat dich bereits aufgefordert. Nimm an</li>";
+											$check = 1;
+										}
+									}
+									else
+									{
+										//Dododo
+									}
+								}
 
-                                foreach($array4 as &$userWeAreAlreadyFightingWith)
-                                {
-                                    if($user['id'] == $userWeAreAlreadyFightingWith['membersid'])
-                                    {
-                                        //User schon herausgefordert
-                                        if($check == 0)
-                                        {
-                                            $check = 1;
-                                            //echo "<li>".$userWeAreAlreadyFightingWith['username']." hat dich bereits geaufgefordert. Nimm an</li>";
-                                        }
-                                    }
-                                    else
-                                    {
-                                        //Dododo
-                                    }
-                                }
+								foreach($array4 as &$userWeAreAlreadyFightingWith)
+								{
+									if($user['id'] == $userWeAreAlreadyFightingWith['membersid'])
+									{
+										//User schon herausgefordert
+										if($check == 0)
+										{
+											$check = 1;
+											//echo "<li>".$userWeAreAlreadyFightingWith['username']." hat dich bereits geaufgefordert. Nimm an</li>";
+										}
+									}
+									else
+									{
+										//Dododo
+									}
+								}
 
 								if($check == 0)
 								{
@@ -344,12 +370,12 @@ include ("include/navbar.php");
 											".$user['username']."
 										</td>
 										<td class='table_style_uebersicht'>
-                                            <form class='form-inline' action='include/functions_quiz.php' method='post'>
-										        <input type='hidden' name='challengeUser' value='true'>
-										        <input type='hidden' name='challengerUserID' value='".$_SESSION['user_id']."'>
-										        <input type='hidden' name='challengedUserID' value='".$user['id']."'>
-										        <input  class='btn-uebersicht btn-uebersicht-green' type='submit' value='Fordere ihn heraus!'>
-									        </form>
+											<form class='form-inline' action='include/functions_quiz.php' method='post'>
+												<input type='hidden' name='challengeUser' value='true'>
+												<input type='hidden' name='challengerUserID' value='".$_SESSION['user_id']."'>
+												<input type='hidden' name='challengedUserID' value='".$user['id']."'>
+												<input  class='btn-uebersicht btn-uebersicht-green' type='submit' value='Fordere ihn heraus!'>
+											</form>
 										</td>
 									</tr>
 									";
@@ -361,7 +387,6 @@ include ("include/navbar.php");
 
 							}
 						}
-
                         echo "</tbody> </table>";
                     ?>
                     
@@ -381,6 +406,125 @@ include ("include/navbar.php");
         <!-- Scripts -->
         <script src="../js/jquery-2.2.2.min.js"></script>
         <script src="../js/bootstrap.min.js"></script>
+		<script type="text/javascript">	
+			$(document).ready(function () {				
+				autocompleteMemberSearch(document.getElementById('input_category'), javascript_array);
+			});
+
+
+			function autocompleteMemberSearch(inp, userarray) {
+				inp.addEventListener("input", function (e) {
+					var i, val = this.value;
+
+					closeAllLists();
+					if (!val) {
+						resetList();
+						return false;
+					}
+
+					var count = 0;
+					var check = 0;
+
+					userarray.forEach(user => {
+						if (user.username.toUpperCase().includes(val.toUpperCase())) {
+							if(sessionID != user.id) {
+								alreadyChallengedUsers_array.forEach(alreadyChallengedUser => {
+									if(user.id == alreadyChallengedUser.userID2) {
+										if(check == 0) {
+											count++;
+											$("#table_user").find('tbody').append("<tr><td>"+count+"</td><td>"+ user.username+"</td><td class='table_style_uebersicht'>Warte auf Annahme</td></tr>");
+											check = 1;
+										}
+									}
+								});
+
+								pendingChallengesUsers_array.forEach(userChallengesUs => {
+									if(user.id == userChallengesUs.userID1) {
+										if(check == 0) {
+											check == 1;
+										}						
+									}
+								});
+
+								currentMPGamesForChallenges_array.forEach(userWeAreAlreadyFightingWith => {
+									if(user.id == userWeAreAlreadyFightingWith.membersid) {
+										if(check == 0) {
+											check = 1;
+										}
+									}
+								});
+
+								if(check == 0) {
+									count++;
+									$("#table_user").find('tbody').append("<tr><td>"+count+"</td><td>"+user.username+"</td><td class='table_style_uebersicht'><form class='form-inline' action='include/functions_quiz.php' method='post'><input type='hidden' name='challengeUser' value='true'><input type='hidden' name='challengerUserID' value='"+sessionID+"'><input type='hidden' name='challengedUserID' value='"+user.id+"'><input  class='btn-uebersicht btn-uebersicht-green' type='submit' value='Fordere ihn heraus!'></form></td></tr>");
+								}
+								else {
+									check = 0;
+								}
+							}
+							else {
+								// Bei selbstfund nichts tun
+							}
+						}								
+					});
+				});
+
+				function closeAllLists(elmnt) {
+					//resets shown List
+					$("#table_user tbody tr").remove()
+				}
+
+				function resetList() {
+					var count = 0;
+					var check = 0;
+
+					userarray.forEach(user => {
+						if(sessionID != user.id) {
+							alreadyChallengedUsers_array.forEach(alreadyChallengedUser => {
+								if(user.id == alreadyChallengedUser.userID2) {
+									if(check == 0) {
+										count++;
+										$("#table_user").find('tbody').append("<tr><td>"+count+"</td><td>"+ user.username+"</td><td class='table_style_uebersicht'>Warte auf Annahme</td></tr>");
+										check = 1;
+									}
+								}
+							});
+
+							pendingChallengesUsers_array.forEach(userChallengesUs => {
+								if(user.id == userChallengesUs.userID1) {
+									if(check == 0) {
+										check == 1;
+									}						
+								}
+							});
+
+							currentMPGamesForChallenges_array.forEach(userWeAreAlreadyFightingWith => {
+								if(user.id == userWeAreAlreadyFightingWith.membersid) {
+									if(check == 0) {
+										check = 1;
+									}
+								}
+							});
+
+							if(check == 0) {
+								count++;
+								$("#table_user").find('tbody').append("<tr><td>"+count+"</td><td>"+user.username+"</td><td class='table_style_uebersicht'><form class='form-inline' action='include/functions_quiz.php' method='post'><input type='hidden' name='challengeUser' value='true'><input type='hidden' name='challengerUserID' value='"+sessionID+"'><input type='hidden' name='challengedUserID' value='"+user.id+"'><input  class='btn-uebersicht btn-uebersicht-green' type='submit' value='Fordere ihn heraus!'></form></td></tr>");
+							}
+							else {
+								check = 0;
+							}						
+						}
+						else {
+							// Bei selbstfund nichts tun
+						}
+					});
+				}
+			}
+
+		</script>
+
         </div>
+
+
 </body>
 </html>
