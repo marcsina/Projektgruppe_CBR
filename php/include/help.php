ead200df17b4886a918308f5bcbfeb2da37e216a
Marc Wildermuth <marc61192@gmail.com>
2018-10-02 13:24:57 +0200

    new help texts different color/background for chat/help

--- php/include/help.php
+++ php/include/help.php
@@ -1,4 +1,4 @@
-<?php
+ï»¿<?php
 include_once 'conn.php';
 include_once 'functions_login.php';
 
@@ -41,36 +41,48 @@ if (login_check($mysqli) == true) {
                 <!--TODODODODOD-->
                 <?php
 
-                $hilfeTextIndex = "Der Beginn deines großen Abenteuers auf der MedAsubild.de Seite.
+                $hilfeTextIndex = "Der Beginn deines groÃŸen Abenteuers auf der MedAsubild.de Seite.
                                    <br>
                                    <br>
                                    Hier siehst du welche neuen Artikel und Forumsposts es in dieser Woche gab und kannst dich direkt zu Ihnen begeben.";
 
-                $hilfeTextQuizUebersicht = "Hilfe Text für die Quiz_ueberischt seite";
-                $hilfeTextQuiz = "Hilfe Text für die Quiz_ueberischt seite";
-                $hilfeTextQuizEndseite = "Hilfe Text für die Quiz_Endseite seite";
-                $hilfeTextForum = "Hilfe Text für die Forum seite";
-                $hilfeTextForumDemenz = "Hilfe Text für die Forum Demenz seite";
-                $hilfeTextForumDemenzTopic = "Hilfe Text für Forum Topic Seite";
-                $hilfeTextCheckerSymptom = "Auf dieser Seite können sie verschiedene Symptome auswählen und ihre Stärke bestimmen, um am Ende das System die passende Krankheit finden zu lassen.<br><br>
-				Auf der linken Seite befindet sich eine Liste allen möglichen Symptomen. Ein Suchfeld direkt dort drüber kann beim Finden eines bestimmten Symptoms helfen. Möchten sie ein Symptom auswählen, klicken sie auf die entsprechende weiße Box. Dieses Symptom erscheint nun auf der rechten Seite<br><br>
-				Mithilfe der rechten Seite können sie die Stärke des von ihnen gewählten Symptoms bestimmen: Klein, Mittel und hoch. Als standard bekommt jedes neue Symptome eine kleine Stärke. Möchten sie ein Symptom entfernen, drücken sie auf das weiße X oder klicken sie erneut auf die weiße Box im linken Fenster (dort ist nun ein Häkchen drin).<br><br>
-				Haben sie ihre Auswahl beendet, klicken sie auf den 'Submit' Button. Im Anschluss sehen sie weiter unten ein Diagramm, welches ihnen das Ergebnis anzeigt. Berühren sie mit ihrem Cursor einen Balken, um die genaue Übereinstimmung festzustellen.<br><br>
-				Sie können jederzeit neue Symptome hinzufügen oder entfernen und anschließend das Ergebnis neu berechnen lassen.";
-                $hilfeTextCheckerText = "Auf dieser Seite können sie eine Krankheitsbeschreibung eingeben um das System die passende Krankheit finden zu lassen.<br><br>
-				Auf der linken Seite befindet sich eine Textbox, in der sie einen beliebig langen Text eingeben können. Lassen sie bei der Beschreibung möglichst keine Details aus, um ein genaues Ergebnis zu erhalten.<br><br>
-				Haben sie ihre Beschreibung abgeschlossen, klicken sie auf den Submit Button. Das System filtert nun ihren Text nach Symptomen, die gefundenen werden ihnen zusammen mit der Stärke auf der rechten Seite angezeigt. Weiter unten befindet sich ein Diagramm, welches ihnen das Ergebnis anzeigt. Berühren sie mit ihrem Cursor einen Balken, um die genaue Übereinstimmung festzustellen.<br><br>
-				Falls sie weitere Symptome hinzufügen oder vorhandene entfernen möchten, klicken sie auf den 'Anpassen' Button.";
-                $hilfeTextArtikel ="Hilfe Text für Artikel Seite";
-                $hilfeTextArtikelShow ="Hilfe Text für Artikel Show Seite";
-                $hilfeTextScripts ="Hilfe Text für Scripts Seite";
-                $hilfeTextStatistik ="Hilfe Text für Statistik Seite";
-                $hilfeTextKontakt ="Hilfe Text für Kontakt Seite";
-                $hilfeTextProfilOwn ="Hilfe Text für eigene Profil Seite";
-                $hilfeTextProfilOther = "Hilfe Text für andere Profil Seite";
-                $hilfeTextAdmin = "Hilfe Text für Admin Seite";
-                $hilfeTextLogin = "Hilfe Text für Login Seite";
-                $hilfeTextRegister = "Hilfe Text für Register Seite";
+                $hilfeTextQuizUebersicht = "Willkommen beim Quiz!
+                                            <br>
+                                            Hier hast du die Auswahl zwischen dem Singleplayer und Multiplayer Modus. Im Singleplayer kannst du deine Skills weiter ausbauen und damit deine Freunde im Multiplayer besiegen!
+                                            <br>
+                                            In jedem Modus bekommst Du 4 Fragen mit je 4 AntwortmÃ¶glichkeiten und am Ende des Quiz eine Auswertung der Spielrunde.";
+                $hilfeTextQuiz = "Die zu beantwortende Frage steht oben, wÃ¤hle nun eine der 4 Antworten aus, welche dir als richtig erscheint um mit dem Quiz fortzufahren.";
+                $hilfeTextQuizEndseite = "Wie hast du abgeschnitten?
+                                          <br>
+                                          Wie schlÃ¤gst du dich gegen deine Freunde im Quiz?
+                                          <br>
+                                          Um die Antworten auf diese Fragen zu erhalten, bist du hier genau richtig!";
+                $hilfeTextForum = "Das Forum bietet Dir die MÃ¶glichkeit mit Dich mit anderen auszutauschen.
+                                    <br>
+                                    Dabei gibt es jeweils ein Oberthema und dieses wird unterteilt, so dass es fÃ¼r verschiedene Unterthemen, je ein eigenes Verzeichnis gibt. Jeder kann DiskussionsbeitrÃ¤ge schreiben, die andere lesen und beantworten kÃ¶nnen.";
+               
+                $hilfeTextForumDemenz = "Hilfe Text fÃ¼r die Forum Demenz seite";
+                $hilfeTextForumDemenzTopic = "Hilfe Text fÃ¼r Forum Topic Seite";
+
+                $hilfeTextCheckerSymptom = "Auf dieser Seite kÃ¶nnen sie verschiedene Symptome auswÃ¤hlen und ihre StÃ¤rke bestimmen, um am Ende das System die passende Krankheit finden zu lassen.<br><br>
+				Auf der linken Seite befindet sich eine Liste allen mÃ¶glichen Symptomen. Ein Suchfeld direkt dort drÃ¼ber kann beim Finden eines bestimmten Symptoms helfen. MÃ¶chten sie ein Symptom auswÃ¤hlen, klicken sie auf die entsprechende weiÃŸe Box. Dieses Symptom erscheint nun auf der rechten Seite<br><br>
+				Mithilfe der rechten Seite kÃ¶nnen sie die StÃ¤rke des von ihnen gewÃ¤hlten Symptoms bestimmen: Klein, Mittel und hoch. Als standard bekommt jedes neue Symptome eine kleine StÃ¤rke. MÃ¶chten sie ein Symptom entfernen, drÃ¼cken sie auf das weiÃŸe X oder klicken sie erneut auf die weiÃŸe Box im linken Fenster (dort ist nun ein HÃ¤kchen drin).<br><br>
+				Haben sie ihre Auswahl beendet, klicken sie auf den 'Submit' Button. Im Anschluss sehen sie weiter unten ein Diagramm, welches ihnen das Ergebnis anzeigt. BerÃ¼hren sie mit ihrem Cursor einen Balken, um die genaue Ãœbereinstimmung festzustellen.<br><br>
+				Sie kÃ¶nnen jederzeit neue Symptome hinzufÃ¼gen oder entfernen und anschlieÃŸend das Ergebnis neu berechnen lassen.";
+                $hilfeTextCheckerText = "Auf dieser Seite kÃ¶nnen sie eine Krankheitsbeschreibung eingeben um das System die passende Krankheit finden zu lassen.<br><br>
+				Auf der linken Seite befindet sich eine Textbox, in der sie einen beliebig langen Text eingeben kÃ¶nnen. Lassen sie bei der Beschreibung mÃ¶glichst keine Details aus, um ein genaues Ergebnis zu erhalten.<br><br>
+				Haben sie ihre Beschreibung abgeschlossen, klicken sie auf den Submit Button. Das System filtert nun ihren Text nach Symptomen, die gefundenen werden ihnen zusammen mit der StÃ¤rke auf der rechten Seite angezeigt. Weiter unten befindet sich ein Diagramm, welches ihnen das Ergebnis anzeigt. BerÃ¼hren sie mit ihrem Cursor einen Balken, um die genaue Ãœbereinstimmung festzustellen.<br><br>
+				Falls sie weitere Symptome hinzufÃ¼gen oder vorhandene entfernen mÃ¶chten, klicken sie auf den 'Anpassen' Button.";
+                $hilfeTextArtikel ="Hier hast du die MÃ¶glichkeit verschiedene Artikel abzurufen, die Dir beim Lernen helfen kÃ¶nnen.";
+                $hilfeTextArtikelShow ="Hier wird dir der zuvor ausgewÃ¤hlte Artikel angezeigt.";
+                $hilfeTextScripts ="Hier hast du die MÃ¶glichkeit verschiedene Skripte abzurufen, die Dir beim Lernen helfen kÃ¶nnen.";
+                $hilfeTextStatistik ="Die Statistik Seite bietet Dir einen Ãœberblick Ã¼ber das Nutzungsverhalten der User.";
+                $hilfeTextKontakt ="Wenn du mit uns Kontakt aufnhemen willst, bist du heir genau richtig. WÃ¤hle eine der kommunikationsanÃ¤le aus und melde ich bei uns, wenn du Fragen oder Anregungen hast.";
+                $hilfeTextProfilOwn ="Auf der Profilseite kannst du eine Beschreibung Ã¼ber Dich hinterlassen, sehen welchen Leuten du folgst und wer Dir folgt. ZusÃ¤tzlich kannst du deine AktivitÃ¤ten und Statistik abrufen.";
+                $hilfeTextProfilOther = "Auf der Profilseite kannst du die AktivÃ¤ten der Person sehen und ihr folgen um gegen sie spannende Quizduelle auszutragen u";
+                $hilfeTextAdmin = "Als Admin kannst du iher neue Cases fÃ¼r die Datenbank anlegen, Neue Kategorien und Symptome hinzufÃ¼gen und ebenso wieder lÃ¶schen.";
+                $hilfeTextLogin = "Auf dieser Seite kannst du dich auf der Medausbild Seite einloggen.";
+                $hilfeTextRegister = "Hier kannst du ein neues Konto erstellen um Teil der Medausbild Community zu werden.";
 
 
                 if(strpos($_SERVER['REQUEST_URI'], "index.php"))
